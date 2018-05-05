4bc7b404b8af16dbebe94c3105102b7a3b2de7d9
Saravana Perumal Shanmugam <saravanaperumal@msn.com>
2018-05-05 10:15:36 -0700

    gateway service

---
+++ service-gateway/src/main/java/com/yosanai/spring/cloud/starter/servicegateway/ServiceGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.yosanai.spring.cloud.starter.servicegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ServiceGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceGatewayApplication.class, args);
+	}
+	
+}
