baef5b5ad9137df7e869ac036bfa2c2926d8dc7e
Saravana Perumal Shanmugam <saravanaperumal@msn.com>
2018-05-10 12:07:20 -0700

    web to registry

--- sample-spring-web/src/main/java/com/yosanai/spring/cloud/starter/samplespringweb/SampleSpringWebApplication.java
+++ sample-spring-web/src/main/java/com/yosanai/spring/cloud/starter/samplespringweb/SampleSpringWebApplication.java
@@ -2,11 +2,13 @@ package com.yosanai.spring.cloud.starter.samplespringweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 import nz.net.ultraq.thymeleaf.LayoutDialect;
 import nz.net.ultraq.thymeleaf.decorators.strategies.GroupingStrategy;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SampleSpringWebApplication {
 
